f989475598c16d6af948c86deef59a1518b8a07c
CodinPetr <petrshalyapin@gmail.com>
2021-02-17 21:46:47 -0500

    added container and button, played with flex-wrap

--- variables.js
+++
@@ -1,6 +0,0 @@
-function signIn() {
-    if (document.querySelector('h2').innerHTML === "Welcome to Lumper Pay!") {
-        document.querySelector('h2').innerHTML = "Glad you are Here!";
-    }
-}
-
